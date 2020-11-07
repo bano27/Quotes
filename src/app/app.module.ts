2a6365160e9f372f98b0aa60a40fbf9ce24248a9
bano27 <thairukibe2798@gmail.com>
2020-11-08 00:04:58 +0300

    generated a quote-details component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DisplayQuoteComponent } from './display-quote/display-quote.component';
+import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DisplayQuoteComponent
+    DisplayQuoteComponent,
+    QuoteDetailsComponent
   ],
   imports: [
     BrowserModule,
