9a73a9572c41bcb40bca8b965144bf8bbf2b8cbb
bano27 <thairukibe2798@gmail.com>
2020-11-09 02:49:42 +0300

    posted date custom pipe update

--- src/app/display-quote/display-quote.component.ts
+++ src/app/display-quote/display-quote.component.ts
@@ -9,15 +9,15 @@ import { MyQuotes } from '../my-quotes';
 export class DisplayQuoteComponent implements OnInit {
 
   quotes: MyQuotes[] = [
-    {Postby: 'Ben', Quote: '“Challenges are what make life interesting and overcoming them is what makes life meaningful.”', Author: 'Joshua J. Marine', Posted: (2005,7,9) },
-    {Postby: 'Susan', Quote: '“Let us always meet each other with smile, for the smile is the beginning of love.”', Author: 'Mother Teresa', Posted: (2011,11,23)},
-    {Postby: 'Jeff', Quote: '“Remember that the happiest people are not those getting more, but those giving more.”', Author: 'Jackson Brown Jr', Posted: (2013,5,14) }
+    {Postby: 'Ben', Quote: '“Challenges are what make life interesting and overcoming them is what makes life meaningful.”', Author: 'Joshua J. Marine', Posted: "2005-10-23"},
+    {Postby: 'Susan', Quote: '“Let us always meet each other with smile, for the smile is the beginning of love.”', Author: 'Mother Teresa', Posted: "2020-03-13"},
+    {Postby: 'Jeff', Quote: '“Remember that the happiest people are not those getting more, but those giving more.”', Author: 'Jackson Brown Jr', Posted: "2019-06-27"}
   ];
 
   addNewQuote(quote){
     let quoteLength = this.quotes.length;
     quote.id = quoteLength+1;
-    quote.completeDate = new Date(quote.completeDate)
+    quote.Posted = new Date(quote.Posted)
     this.quotes.push(quote)
   }
 
